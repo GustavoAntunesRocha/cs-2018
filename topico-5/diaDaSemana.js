715a0fe6a60c1ff5ff822749e28045c0365e6a29
Gustavo Antunes <gustavorocha@inf.ufg.br>
2018-05-09 20:24:23 -0300

    Adicionado comentarios e criado o metodos diaDaSemana

--- topico-5/diaDaSemana.js
+++ topico-5/diaDaSemana.js
@@ -1,4 +1,14 @@
 /** @namespace */
 class DiaDaSemana{
-    
+    /**
+     * Um algoritmo que define o dia da semana para uma determinada data de Kim Larsen
+     * @param {number} d Dia
+     * @param {number} m Mes
+     * @param {number} a Ano
+     * 
+     * @returns {number} O dia da semana, 0 para segunda, 1 para terça, e assim por diante
+     */
+    diaDaSemana(d,m,a){
+        
+    }
 }
