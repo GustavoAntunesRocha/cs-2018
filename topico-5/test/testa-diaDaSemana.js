3640e83d118fa3c3f058cd234d846896a4cdf149
Gustavo Antunes <gustavorocha@inf.ufg.br>
2018-05-09 21:17:52 -0300

    Terminado arquivo de teste

--- topico-5/test/testa-diaDaSemana.js
+++ topico-5/test/testa-diaDaSemana.js
@@ -1,1 +1,10 @@
 import { DiaDaSemanama } from "../diaDaSemana;
+const dia = new DiaDaSemanama();
+QUnit.test('Dia da Semana', function (assert) {
+
+    // Executa a operação que desejamos testar
+    let resultado = dia.diaDaSemana(9,5,2018);
+  
+    // Verifica se o resultado produzido é o esperado.
+    assert.equal(resultado, 2, 'resultado incorreto');
+  });
