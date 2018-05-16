8815efd9996b1a5a4c59cc13d8b159eff5677396
Gustavo Antunes <gustavorocha@inf.ufg.br>
2018-05-16 19:59:34 -0300

    Criado documentação do jsdoc

--- topico-5/aplicacao.js
+++ topico-5/aplicacao.js
@@ -1,2 +1,6 @@
 import { DiaDaSemana } from "diaDaSemana";
 const diaSemana = new DiaDaSemana();
+const dia = parseInt(process.argv[2]);
+const mes = parseInt(process.argv[3]);
+const ano = parseInt(process.argv[4]);
+console.log(diaSemana.dia(dia,mes,ano));
