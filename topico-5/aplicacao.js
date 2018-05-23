74e281415aed3961f5f9c4023730abede499b91f
Gustavo Antunes <gustavorocha@inf.ufg.br>
2018-05-23 20:05:09 -0300

    Alterado aplicacao

--- topico-5/aplicacao.js
+++ topico-5/aplicacao.js
@@ -3,5 +3,6 @@ const diaSemana = new DiaDaSemana();
 var dia = parseInt(process.argv[2]);
 var mes = parseInt(process.argv[3]);
 var ano = parseInt(process.argv[4]);
+console.log(process.argv[2]);
 console.log(dia,mes,ano);
 console.log(diaSemana.dia(dia,mes,ano));
--- topico-5/aplicacaoNode5.js
+++ topico-5/aplicacaoNode5.js
@@ -194,6 +194,7 @@ var diaSemana = new _diaDaSemana.DiaDaSemana();
 var dia = parseInt(process.argv[2]);
 var mes = parseInt(process.argv[3]);
 var ano = parseInt(process.argv[4]);
+console.log(process.argv[2]);
 console.log(dia, mes, ano);
 console.log(diaSemana.dia(dia, mes, ano));
 
