0e520fc3873ed585201343431c1dd65677220695
Gustavo Antunes <gustavorocha@inf.ufg.br>
2018-05-23 20:12:32 -0300

    Adicionado documentação para jsdoc

--- topico-6/resto.js
+++ topico-6/resto.js
@@ -1,4 +1,11 @@
 /*Gustavo Antunes Rocha*/
+/**
+ * 
+ * @param {number} x 
+ * @param {number} y
+ * 
+ * @returns {number} O resto da divisao inteira de x por y
+ */
 function resto(x,y){
     if(y < 0 || x <= 0){
         return 1;
