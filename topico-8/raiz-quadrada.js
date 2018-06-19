be76ccd7ab7d0b1086af7f51cfcd3b72b2d90882
Gustavo Antunes <gustavorocha@inf.ufg.br>
2018-06-19 14:22:27 -0300

    Feito o algoritmo 13: raiz quadrada

---
+++ topico-8/raiz-quadrada.js
@@ -0,0 +1,18 @@
+/*Gustavo Antunes Rocha*/
+/**
+ * @param {number} n
+ * @param {number} i precisão
+ * @returns {number} A raiz quadrada de n
+ */
+function raiz(n,i){
+    if(n <= 0){
+        return -1;
+    }
+    var r = 1;
+    while(i >= 0){
+        r = (r + n / r) / 2;
+        i--;
+    }
+    return r;
+}
+exports.raiz = raiz;
