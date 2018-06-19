d3bb6ec8c6602e1c78471b0af60b487c7dbbfc94
Gustavo Antunes <gustavorocha@inf.ufg.br>
2018-06-19 10:10:01 -0300

    Criacao do algoritmo 9: Valor de PI

---
+++ topico-8/valor-pi.js
@@ -0,0 +1,22 @@
+/*Gustavo Antunes Rocha*/
+/**
+ * @param {number} n
+ * @returns {number} O valor de pi
+ */
+function pi(n){
+    if(n < 1){
+        return -1;
+    }
+    var i = 1;
+    var s = -1;
+    var d = -1;
+    var p = 0;
+    while(i <= n){
+        d += 2;
+        s *= -1;
+        p += 4 * s / d;
+        i++;
+    }
+    return p;
+}
+exports.pi = pi;
