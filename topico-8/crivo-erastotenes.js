8dba08c8abceb6ade03bc2a94cc4cdaffe38cda3
Gustavo Antunes <gustavorocha@inf.ufg.br>
2018-06-20 15:09:05 -0300

    Feito algoritmo 15: crivo de erastotenes

---
+++ topico-8/crivo-erastotenes.js
@@ -0,0 +1,28 @@
+/*Gustavo Antunes Rocha*/
+/**
+ * @param {number} n
+ * @returns {boolean} Se n é um numero primo
+ */
+function crivoErastotenes(n){
+    a = new Array(n);
+    var i;
+    for(i=0;i<n;i++){
+        a[i] = 0;
+    }
+    var limite = Math.sqrt(n);
+    var multiplo;
+    for(i=2;i <= limite;i++){
+        if(a[i] == 0){
+            multiplo = i + i;
+            while(multiplo <= n){
+                a[multiplo] = 1;
+                multiplo += i;
+            }
+        }
+    }
+    if(a[n] == 0){
+        return true;
+    }
+    return false;
+}
+exports.crivoErastotenes = crivoErastotenes;
